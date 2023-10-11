c74a0e82aa433bd7d7c8e99c5fea0f57984a46f1
Matheus-Nasci <matheus.noliveira@sptech.school>
2023-10-10 21:35:20 -0300

    fix(ViaCep): Alterando endpoint para chamdado serviço

--- src/services/viacep-service.ts
+++ src/services/viacep-service.ts
@@ -6,7 +6,7 @@ const url = process.env.PATH_API_VIACEP;
 
 const BuscarCep = async (cep: string): Promise<ViaCepResponse> => {
     try {
-        const response = await axios.get(url + `${cep}/json/`);
+        const response = await axios.get(url + `/${cep}/json/`);
         return response.data;
     } catch (e) {
         handleError(e)
